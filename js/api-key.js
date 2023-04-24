f5efd50c209c1b13c2f91b261311e9384271dfce
yshy9373 <idealicehee@gmail.com>
2023-04-24 11:27:50 +0900

    Updates

---
+++ js/api-key.js
@@ -0,0 +1,1 @@
+const API_KEY = 'f16cf43ad826795fbd027ef01c6d5abc';
--- js/greetings.js
+++ js/greetings.js
@@ -1,29 +1,28 @@
-const loginForm = document.querySelector("#login-form")
+const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
-const greeting = document.querySelector("#greeting");
+const greeting = document.querySelector('#greeting');
 
-const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "username";
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
 
-function onLoginSubmit(event){
-    event.preventDefault(); 
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
-function paintGreetings(username){
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if(savedUsername === null){
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit)
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', onLoginSubmit);
 } else {
-    paintGreetings(savedUsername);
+  paintGreetings(savedUsername);
 }
